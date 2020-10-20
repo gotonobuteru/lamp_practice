bac644e4c9c7d09b291b57332da1886b9b3ac95a
gotounobuteru <gotonobuteru@gmail.com>
2020-10-20 14:17:12 +0900

    #12

--- www/model/functions.php
+++ www/model/functions.php
@@ -156,3 +156,107 @@ function is_valid_csrf_token($token) {
   //トークンとセッション変数に代入されているトークンの真偽値を返す
   return $token === get_session('csrf_token');
 }
+
+//購入履歴の保存
+function insert_logs($db, $user_id){
+  $sql = "
+    INSERT INTO
+      logs(
+        user_id
+      )
+    VALUES(?);
+  ";
+
+  return execute_query($db, $sql, array($user_id));
+}
+
+//最後に追加したデータのIDを取得
+function get_lastID($db) {
+  return $db->lastInsertID();
+}
+
+//購入履歴明細を保存する
+function insert_logs_info($db, $item_id, $price, $amount, $last_id){
+  $sql = "
+    INSERT INTO
+      logs_info(
+        order_id,
+        item_id,
+        price,
+        amount
+      )
+    VALUES(?, ?, ?, ?);
+  ";
+
+  return execute_query($db, $sql, array($last_id, $item_id, $price, $amount));
+}
+
+//購入履歴と購入履歴明細をトランザクション処理する
+function insert_log_transaction($db, $user_id, $carts) {
+  $err_check = array();
+  
+  // $db->beginTransaction();
+  //購入履歴の保存
+  if (insert_logs($db, $user_id) === false) {
+    $err_check[] = false;
+  }
+  //最後に追加したデータのID取得
+  if (($last_id = get_lastID($db)) === '') {
+    $err_check[] = false;
+  }
+  //↑ここまで正常動作----------------------
+
+  // カートに入っている商品毎SQL文を実行する
+  foreach ($carts as $cart) {
+    if (insert_logs_info($db, $cart['item_id'], $cart['price'], $cart['amount'], $last_id) === false) {
+      $err_check[] = false;
+    }
+  }
+  //SQL文が全てtrueならコミットする
+  // if (count($err_check) === 0) {
+  //   $db->commit();
+  //   return true;
+  // } else {
+  //   $db->rollback();
+  //   return false;
+  // } 
+
+
+
+  // // カートに入っている商品毎SQL文を実行する
+  // foreach ($carts as $cart) {
+  //   $item_id[] = $cart['item_id'];
+  //   $price[] = $cart['price'];
+  //   $amount[] = $cart['amount'];
+  // }
+  // for ($i = 0; $i < count($item_id); $i++) {
+  //   if (insert_logs_info($db, $item_id[$i], $price[$i], $amount[$i], $last_id) === false) {
+  //     $err_check[] = false;
+  //   }
+  // }
+  // //SQL文が全てtrueならコミットする
+  // if (count($err_check) === 0) {
+  //   $db->commit();
+  //   return true;
+  // } else {
+  //   $db->rollback();
+  //   return $item_id;
+  // } 
+
+
+  // //カートに入っている商品毎SQL文を実行する
+  // for ($i = 0; $i < count($carts); $i++) {
+  //   if (insert_logs_info($db, $cart[$i]['item_id'], $cart[$i]['price'], $cart[$i]['amount'], $last_id) === false) {
+  //     $err_check[] = false;
+  //   }
+  // }
+  // // SQL文が全てtrueならコミットする
+  // if (count($err_check) === 0) {
+  //   $db->commit();
+  //   return true;
+  // } else {
+  //   $db->rollback();
+  //   return $err_check;
+  // } 
+
+}
